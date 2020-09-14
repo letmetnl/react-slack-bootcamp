962413d3832f42820fa6ed09f90bc588285db32f
mohits Projects <mohit5150515@gmail.com>
2020-09-14 22:01:34 +0530

    added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { SignIn } from './';
 import { Component } from 'react';
-function App() {
-  return (
-    <div className="App">
-      <SignIn />
-    </div>
-  );
+import { Switch, Route } from 'react-router-dom';
+
+function Home() {
+  return <div>Home</div>;
+}
+function Some() {
+  return <div>Some</div>;
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/some" component={Some} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
