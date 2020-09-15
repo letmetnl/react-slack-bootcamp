3f886a5d74f8232d7d82f10de9227e504e0829de
mohits Projects <mohit5150515@gmail.com>
2020-09-15 20:18:58 +0530

    added styling to slack components

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,11 +7,23 @@ class Sidebar extends Component {
         <div className="user-profile">
           <div>
             <img />
+            <div className="avatar">
+              <img src="https://www.flaticon.com/svg/static/icons/svg/2919/2919600.svg" />
+            </div>
+            <div>Ronald</div>
           </div>
-          <div>Ronald</div>
-        </div>
+          <hr className="sidebar-spacer" />
 
-        <div className="user-channels"></div>
+          <div className="user-channels"></div>
+          <div className="channels">
+            <div className="header">Channels</div>
+
+            <ul className="channels-list">
+              <li># assignment</li>
+              <li># test</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
